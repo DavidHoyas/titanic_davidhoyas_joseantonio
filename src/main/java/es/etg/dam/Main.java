247c87882e6eb8fa97ec839dd19d9dc838ea8a33
DavidHoyas <thetxaby@gmail.com>
2025-10-28 14:47:25 -0500

    Edición de las clases Personas y Main

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -2,21 +2,26 @@ package es.etg.dam;
 
 public class Main {
 
-    public static final int CONTADORBOTES = 20;
+    public static final int CONTADOR_BOTES = 20;
 
     public static void main(String[] args) {
+        // Creamos el servicio de emergencias
         ServicioEmergencia servicio = new ServicioEmergencia(new GeneradorMD());
 
-        for (int i = 0; i < CONTADORBOTES; i++) {
+        // Lanzamos los botes en hilos independientes
+        for (int i = 0; i < CONTADOR_BOTES; i++) {
             String id = String.format("B%02d", i);
             Personas bote = new Personas(id, servicio);
-            bote.run();
+            Thread hilo = new Thread(bote);
+            hilo.start();
         }
 
-        servicio.waitForAllReports(CONTADORBOTES);
+        // Esperamos a que todos los reportes se reciban
+        servicio.waitForAllReports(CONTADOR_BOTES);
 
+        // Generamos el informe final
         servicio.generateReport();
 
-        System.out.println("Informe generado");
+        System.out.println("Informe generado correctamente.");
     }
 }
