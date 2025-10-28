1bac2af33e3dabf29cf34f525ccc6fe044df74bb
Sancha-aesthetic <joseantonio.sanchachausal@educa.madrid.org>
2025-10-28 13:31:13 -0500

    v1

--- src/main/java/es/etg/dam/Main.java
+++ src/main/java/es/etg/dam/Main.java
@@ -1,7 +1,22 @@
 package es.etg.dam;
 
 public class Main {
+
+    public static final int BOAT_COUNT = 20;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmergencyService service = new EmergencyService(new MarkdownReportGenerator());
+
+        for (int i = 0; i < BOAT_COUNT; i++) {
+            String id = String.format("B%02d", i);
+            Boat boat = new Boat(id, service);
+            boat.run();
+        }
+
+        service.waitForAllReports(BOAT_COUNT);
+
+        service.generateReport();
+
+        System.out.println("Informe generado: Informe.md en el directorio de trabajo.");
     }
 }
