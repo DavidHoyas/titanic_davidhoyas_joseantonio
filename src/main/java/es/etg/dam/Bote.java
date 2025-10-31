32b3c17c981d58233110c53d99ad4b24aea945a0
Sancha-aesthetic <joseantonio.sanchachausal@educa.madrid.org>
2025-10-31 08:52:01 -0500

    V12

--- src/main/java/es/etg/dam/Bote.java
+++ src/main/java/es/etg/dam/Bote.java
@@ -9,32 +9,6 @@ import lombok.Data;
 @AllArgsConstructor
 public class Bote {
 
-<<<<<<< HEAD
-    public static final Random RANDOM = new Random();
-
-    private final String id;
-    private final int total;
-    private final int mujeres;
-    private final int hombres;
-    private final int ninios;
-
-    public Bote(String id) {
-        this.id = id;
-
-        this.total = 10 + RANDOM.nextInt(91); // [10, 100]
-
-        this.ninios = RANDOM.nextInt(total / 2 + 1);
-
-        this.mujeres = RANDOM.nextInt(total - ninios + 1);
-
-        this.hombres = total - ninios - mujeres;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Bote %s [Total=%d, Mujeres=%d, Hombres=%d, Niños=%d]",
-                id, total, mujeres, hombres, ninios);
-=======
     public static final String FORMATO_SALIDA = "%s;%d;%d;%d;%d%n";
 
     public static void main(String[] args) {
@@ -51,6 +25,5 @@ public class Bote {
         int hombres = total - ninos - mujeres;
 
         System.out.printf(FORMATO_SALIDA, id, total, mujeres, hombres, ninos);
->>>>>>> 29b4829 (V12)
     }
 }
